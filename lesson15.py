c90204153793f3457d2fdd886c5f3bb44e84bc99
Irinaka Robodone <irinaka.robodone@gmail.com>
2022-10-20 20:06:17 +0900

    init commit by ka

--- lesson15.py
+++ lesson15.py
@@ -1,49 +1,30 @@
+import tkinter
+
+
 import tkinter as tk
-root=tk.Tk()
-root.title('しりとり')
-root.geometry('500x180')
-Label=tk.Label(text='今の文字は「しりとり」です。次の文字を入力してください')
-Label.place(x=100,y=150)
+root=tk.TK()
+root.title('りとりし')
+root.geometry('500x500')
 
-def callback(event):
-    user_input=event.keysym
-    if user_input=="Return":
-        print("enter!")
-        check_siritori(Label["text"])
-root.bind("<key>",callback)
-
-def GetWord():
-    if word_e.get()!='':
-        word=str(word_e.get())
-        return word
-    else:
-        Label["text"]="文字が入力されていません"
-        raise ValueError("文字が入力されていません")
-def check_siritori(word):
-    word=GetWord()
-    if word[0]==word_list[-1][-1]:
-        for words in word_list:
-            if words==word:
-                Label["text"]="重複するワードがあります"
-                raise ValueError("重複するワードがあります")
-        word_list.append(word)
-    else:
-        Label["text"]="しりとりになっていません"
-        raise ValueError("しりとりになっていません")
-    if word[-1]=="ん":
-        Label["text"]="「ん」でおわりました。しりとりを終了"
-        raise ValueError("しりとり終了")
-    print(word_list)
-    Label["text"]='今の文字は「'+word+'」です。次の文字を入力してください'
-    word_e.delete(0,tk.END)
-
+Label  = tk.Label(text='の文字は「しりとり」です。次の文字を入力してください など')
+Label.place(x=10,y=50)
 
 word=0
-word_e=tk.Entry(root,width=25)
+word_e=tk.Entry(root,widith=25)
 word_e.place(x=130,y=50)
-word_list=['しりとり']
+word_list = ['しりとり']
+
 Button=tk.Button(root,text='しりとり（ひらがな）')
 Button.bind("<Button-1>",check_siritori)
-Button.place(x=180,y=100)
 
+def check_siritori(word):
+    word=GetWord()
+    
+    if word[0]==word_list[-1][-1]:
+        for wordsword in word_list:
+            if words==word:
+                """"""
+                Label["text"]="重複するワードがあります。"
+                raise ValueError("重複するワードがあります。")
+        word_list.append(word)
 root.mainloop()
